4b46c18df213016d1714a55424b29ae0bd81a375
nchristowitz <48205934+nchristowitz@users.noreply.github.com>
2025-02-03 22:12:36 +0100

    Initial Commit

---
+++ wevebeenherebeforeTests/wevebeenherebeforeTests.swift
@@ -0,0 +1,17 @@
+//
+//  wevebeenherebeforeTests.swift
+//  wevebeenherebeforeTests
+//
+//  Created by Nicholas Christowitz on 03.02.25.
+//
+
+import Testing
+@testable import wevebeenherebefore
+
+struct wevebeenherebeforeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
